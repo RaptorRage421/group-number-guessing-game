dc8158da218aabf259a881d9db1b872a18e9eace
Sam Shacter <sshacter@gmail.com>
2024-05-09 11:16:56 -0500

    init, installed, got started. get request, now working on post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,42 @@ function onReady() {
 }
 
 onReady()
+
+let guessList = () => {
+  console.log("Guess List is working!")
+  axios({
+      method: 'GET', // HTTP method
+      url: '/guesses'
+  })
+  .then((response) => { // Captures the response from server
+      // Must be response.data
+      let guesses = response.data
+      console.log("guesses are...", guesses)
+  
+          // Render quotes to DOM
+      renderGuesses(guesses)
+  })
+  .catch((error) => { // Manages errors
+      console.log("GET for /guesses didnt work...", error)
+      alert("Oopsie, that didnt work.")
+  })
+}
+guessList()
+
+
+let renderGuesses = (allItems) => {
+  console.log("renderItems is Working...", allItems)
+  let guessOutput = document.getElementById('guesses_go_here')
+  guessOutput.innerHTML = ""
+  for (i=0;i<allItems.length;i++){
+
+      console.log(allItems[i].player1)
+      console.log(allItems[i].player2)
+      guessOutput.innerHTML += `<tr>
+      <th>${allItems[i].player1}</th>
+      <th>${allItems[i].player2}</th>
+    </tr>`
+  }
+}
+
+
--- server/server.js
+++ server/server.js
@@ -5,12 +5,30 @@ const PORT = 5000;
 
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(express.json())
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+const numberStorage = [{player1: 23,
+  player2: 7
+}]
 
+app.get('/guesses', (req,res) => {
+  console.log("Number Guesses...", numberStorage)
+res.send(numberStorage)
+})
+
+
+app.post('/guesses', (req, res) => {
+  let incomingGuess = req.body
+
+  console.log("Goign to add an Item", incomingGuess)
+  numberStorage.push(incomingGuess)
+  console.log(numberStorage)
+res.sendStatus(201)
+
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
